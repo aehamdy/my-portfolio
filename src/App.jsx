6a57b66694ae9597b8ac2ec2533fe36afa2869f2
aehamdy <ae.hamdy91@gmail.com>
2024-10-18 19:22:13 +0300

    chore: Add main color variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Hello there!</h1>
-    </>
+    <section className="dark">
+      <h1 className="text-heading">Hello there!</h1>
+      <p className="text-icon">ICON</p>
+      <p className="text-paragraph">
+        <strong className="text-heading block">Lorem ipsum dolor</strong>
+        sit amet consectetur adipisicing elit. Praesentium impedit voluptates
+        recusandae! Harum, qui dolores doloribus repellendus corrupti illo quod
+        temporibus quae perferendis amet ab voluptas, iure, aperiam dolorem sed.
+        Sit, aperiam. Quia nemo quisquam sunt, tenetur labore soluta accusamus
+        beatae aut eveniet asperiores! Laboriosam aut possimus, unde enim
+        tempore mollitia deleniti et quis repudiandae veritatis? Inventore
+        voluptas laborum doloribus.
+      </p>
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: "selector",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#f6b846',
+        heading: 'var(--heading-color)',
+        paragraph: 'var(--paragraph-color)',
+        button: 'var(--button-color)',
+        icon: 'var(--icon-color)',
+      }
+    },
   },
   plugins: [],
 }
