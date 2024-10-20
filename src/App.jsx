2be2c2c185cef2514d3d6cda2d1298dc7b2a57e5
aehamdy <ae.hamdy91@gmail.com>
2024-10-20 17:55:32 +0300

    style: Add custom border to SectionTitle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "./components/HeroSection";
 import "./App.css";
 import Header from "./components/Header";
+import SectionTitle from "./components/SectionTitle";
 
 function App() {
   return (
     <div className="container mx-auto">
       <Header />
       <HeroSection />
+      <SectionTitle title="about me" />
     </div>
   );
 }
