339d6425c13d3514491763e8b28916d6336cb2b1
aehamdy <ae.hamdy91@gmail.com>
2024-10-22 21:27:36 +0300

    style: Style NoProjectsFound component

--- src/components/NoProjectsFound.jsx
+++ src/components/NoProjectsFound.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable react/prop-types */
+import { TbMoodSadDizzy } from "react-icons/tb";
+
 function NoProjectsFound({ selectedCategory }) {
   return (
-    <p className="text-light-gray">
-      {`No projects added for '${
-        selectedCategory.charAt().toUpperCase() +
-        selectedCategory.slice(1).toLowerCase()
-      }' category yet`}
-    </p>
+    <div className="flex justify-center items-center gap-2">
+      <p className="text-light-gray">
+        {`No projects added for `}
+        <span className="text-sm text-accent font-medium">
+          {selectedCategory.charAt(0).toUpperCase() +
+            selectedCategory.slice(1).toLowerCase()}
+        </span>
+        {` category yet `}
+      </p>
+      <span className="text-xl text-light-gray">
+        <TbMoodSadDizzy />
+      </span>
+    </div>
   );
 }
 
