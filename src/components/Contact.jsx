502b63d74d1bb350bc8c7899a478e05b1979e0eb
aehamdy <ae.hamdy91@gmail.com>
2024-10-23 02:32:15 +0300

    feat: Create Form component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import Form from "./Form";
 import GetInTouchList from "./GetInTouchList";
 import Map from "./Map";
 import SectionTitle from "./SectionTitle";
@@ -9,9 +10,18 @@ function Contact() {
       className="py-main-section-padding bg-section-light dark:bg-section-dark rounded-main-section"
     >
       <SectionTitle title="get in touch" />
+
       <div className="flex flex-col items-start gap-7 mt-section-margin px-section-padding">
         <Map />
         <GetInTouchList />
+      </div>
+
+      <div className="mt-section-margin">
+        <SectionTitle title="contact form" />
+      </div>
+
+      <div className="mt-section-margin px-section-padding">
+        <Form />
       </div>
     </section>
   );
---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+function Form() {
+  return (
+    <form action="">
+      <label htmlFor="name">
+        <input type="text" name="name" id="name" />
+      </label>
+    </form>
+  );
+}
+
+export default Form;
