dd6c780a94520ba3d65be51ce9fc38949444bf6d
aehamdy <ae.hamdy91@gmail.com>
2024-10-22 15:56:22 +0300

    feat: Create projects js file that contains all projects

---
+++ src/data/projects.js
@@ -0,0 +1,67 @@
+import project1 from "../assets/projects/project1.jpg";
+import project2 from "../assets/projects/project2.jpg";
+import project3 from "../assets/projects/project3.jpg";
+import project4 from "../assets/projects/project4.jpg";
+import project5 from "../assets/projects/project5.jpg";
+import project6 from "../assets/projects/project6.jpg";
+import project7 from "../assets/projects/project7.jpg";
+import project8 from "../assets/projects/project8.jpg";
+
+export const projects = [
+    {
+        image: project1,
+        name: '',
+        description: '',
+        link: '',
+        type: '',
+    },
+    {
+        image: project2,
+        name: '',
+        description: '',
+        link: '',
+        type: '',
+    },
+    {
+        image: project3,
+        name: '',
+        description: '',
+        link: '',
+        type: '',
+    },
+    {
+        image: project4,
+        name: '',
+        description: '',
+        link: '',
+        type: '',
+    },
+    {
+        image: project5,
+        name: '',
+        description: '',
+        link: '',
+        type: '',
+    },
+    {
+        image: project6,
+        name: '',
+        description: '',
+        link: '',
+        type: '',
+    },
+    {
+        image: project7,
+        name: '',
+        description: '',
+        link: '',
+        type: '',
+    },
+    {
+        image: project8,
+        name: '',
+        description: '',
+        link: '',
+        type: '',
+    },
+]
