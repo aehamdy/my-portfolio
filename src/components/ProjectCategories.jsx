6e0bd9b30e3a56111a8a5725f30cf27cf2d8652b
aehamdy <ae.hamdy91@gmail.com>
2024-10-22 17:45:06 +0300

    style: Add styles to ProjectCategories according to UI design

--- src/components/ProjectCategories.jsx
+++ src/components/ProjectCategories.jsx
@@ -2,10 +2,10 @@ import { projectCategories } from "../data/projects";
 
 function ProjectCategories() {
   const handleChange = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
   };
   return (
-    <div className="flex">
+    <div className="flex gap-2">
       {projectCategories.map((item, i) => (
         <div key={i}>
           <input
@@ -14,8 +14,12 @@ function ProjectCategories() {
             id={item}
             value={item}
             onClick={handleChange}
+            className="hidden peer"
           />
-          <label htmlFor={item}>
+          <label
+            htmlFor={item}
+            className="font-normal text-sm text-dark-gray dark:text-white-85 peer-checked:text-accent duration-short"
+          >
             {item.charAt(0).toUpperCase() + item.slice(1)}
           </label>
         </div>
