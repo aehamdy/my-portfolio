3abce4a9004c2c467a1ef2ba1e7fdcd1fc70438f
aehamdy <ae.hamdy91@gmail.com>
2024-10-22 17:49:46 +0300

    feat: Set default checked attribute to the input of 'all' value

--- src/components/ProjectCategories.jsx
+++ src/components/ProjectCategories.jsx
@@ -14,6 +14,7 @@ function ProjectCategories() {
             id={item}
             value={item}
             onClick={handleChange}
+            defaultChecked={item === "all"}
             className="hidden peer"
           />
           <label
