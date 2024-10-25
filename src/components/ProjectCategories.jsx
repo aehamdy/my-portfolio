3b6c2c8cbf9c9ebe5e4ef8500845af1d97473dce
aehamdy <ae.hamdy91@gmail.com>
2024-10-25 21:43:23 +0300

    refactor: Create ProjectCategoriesButton to be stand alone component and isolate it from ProjectCategories

--- src/components/ProjectCategories.jsx
+++ src/components/ProjectCategories.jsx
@@ -1,31 +1,16 @@
 /* eslint-disable react/prop-types */
 import { projectCategories } from "../data/projects";
+import ProjectCategoriesButton from "./ProjectCategoriesButton";
 
 function ProjectCategories({ setSelectedCategory }) {
-  const handleChange = (e) => {
-    setSelectedCategory(e.target.value);
-  };
-
   return (
     <div className="flex gap-2 ms-2 pt-[12.5px] px-section-padding">
       {projectCategories.map((item, i) => (
-        <div key={i}>
-          <input
-            type="radio"
-            name="project-category"
-            id={item}
-            value={item}
-            onChange={handleChange}
-            defaultChecked={item === "all"}
-            className="hidden peer"
-          />
-          <label
-            htmlFor={item}
-            className="font-normal text-sm text-dark-gray dark:text-white-85 hover:text-accent peer-checked:text-accent cursor-pointer duration-300"
-          >
-            {item.charAt(0).toUpperCase() + item.slice(1)}
-          </label>
-        </div>
+        <ProjectCategoriesButton
+          key={i}
+          item={item}
+          setSelectedCategory={setSelectedCategory}
+        />
       ))}
     </div>
   );
---
+++ src/components/ProjectCategoriesButton.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+
+function ProjectCategoriesButton({ item, setSelectedCategory }) {
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="radio"
+        name="project-category"
+        id={item}
+        value={item}
+        onChange={handleChange}
+        defaultChecked={item === "all"}
+        className="hidden peer"
+      />
+      <label
+        htmlFor={item}
+        className="font-normal text-sm text-dark-gray dark:text-white-85 hover:text-accent peer-checked:text-accent cursor-pointer duration-300"
+      >
+        {item.charAt(0).toUpperCase() + item.slice(1)}
+      </label>
+    </div>
+  );
+}
+
+export default ProjectCategoriesButton;
