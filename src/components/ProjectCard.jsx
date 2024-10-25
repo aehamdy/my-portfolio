383e4e0db06ce02d64fd632551e43caa6071056a
aehamdy <ae.hamdy91@gmail.com>
2024-10-25 21:32:20 +0300

    feat: Create ProjectCard component and isolate it from ProjectList component

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+
+function ProjectCard({ key, project }) {
+  return (
+    <article key={key} className="group w-full">
+      <a href={project.link} target="_blank" className="flex flex-col gap-2">
+        <div className="w-full rounded-[25px] overflow-hidden">
+          <img
+            src={project.image}
+            alt={project.altText}
+            style={{ width: "100%" }}
+            className="group-hover:scale-110 duration-short"
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <h4 className="font-semibold text-base text-dark-gray dark:text-white-90 group-hover:text-accent duration-short">
+            {project.name}
+          </h4>
+
+          <span className="text-sm text-light-gray">
+            {project.description.charAt(0).toUpperCase() +
+              project.description.slice(1).toLowerCase()}
+          </span>
+        </div>
+      </a>
+    </article>
+  );
+}
+
+export default ProjectCard;
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { projects } from "../data/projects";
 import NoProjectsFound from "./NoProjectsFound";
+import ProjectCard from "./ProjectCard";
 
 function ProjectList({ selectedCategory }) {
   const projectsArray = [];
@@ -26,33 +27,7 @@ function ProjectList({ selectedCategory }) {
     >
       {filteredProjects.length > 0 ? (
         filteredProjects.map((project, i) => (
-          <article key={i} className="group w-full">
-            <a
-              href={project.link}
-              target="_blank"
-              className="flex flex-col gap-2"
-            >
-              <div className="w-full rounded-[25px] overflow-hidden">
-                <img
-                  src={project.image}
-                  alt={project.altText}
-                  style={{ width: "100%" }}
-                  className="group-hover:scale-110 duration-short"
-                />
-              </div>
-
-              <div className="flex flex-col gap-1">
-                <h4 className="font-semibold text-base text-dark-gray dark:text-white-90 group-hover:text-accent duration-short">
-                  {project.name}
-                </h4>
-
-                <span className="text-sm text-light-gray">
-                  {project.description.charAt(0).toUpperCase() +
-                    project.description.slice(1).toLowerCase()}
-                </span>
-              </div>
-            </a>
-          </article>
+          <ProjectCard key={i} project={project} />
         ))
       ) : (
         <NoProjectsFound selectedCategory={selectedCategory} />
