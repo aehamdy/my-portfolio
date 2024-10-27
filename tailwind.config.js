4c4d4c12aa7de0c043425f9ad12bf32c454ffb9e
aehamdy <ae.hamdy91@gmail.com>
2024-10-27 15:36:41 +0300

    feat: Replace darkMode in tailwind config file from 'class' to 'selector'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  darkMode: "class",
+  darkMode: "selector",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
