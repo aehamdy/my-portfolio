f6fe5cea33ba84d7532f49910b71869d82b7862a
aehamdy <ae.hamdy91@gmail.com>
2024-10-31 19:09:46 +0300

    feat: Update ThemeSwitcher component to dynamically set favicon for dark and light themes

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -3,29 +3,43 @@ import { useState, useEffect } from "react";
 
 function ThemeSwitcher() {
   const [isDark, setIsDark] = useState(true);
+  const localStorageKey = "AETheme";
+
+  const updateFavicon = (isDark) => {
+    // Update all favicon links
+    document.querySelectorAll("link[rel='icon']").forEach((favicon) => {
+      favicon.href = isDark ? "/favicon-dark.ico" : "/favicon-light.ico";
+    });
+  };
 
   const changeTheme = () => {
-    setIsDark((prevValue) => !prevValue);
+    setIsDark((prevValue) => {
+      const newIsDark = !prevValue;
 
-    const bodyElement = document.body;
-    if (isDark) {
-      bodyElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    } else {
-      bodyElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    }
+      const bodyElement = document.body;
+      if (newIsDark) {
+        bodyElement.classList.add("dark");
+        localStorage.setItem(localStorageKey, "dark");
+      } else {
+        bodyElement.classList.remove("dark");
+        localStorage.setItem(localStorageKey, "light");
+      }
+
+      // Update the favicon based on the new theme
+      updateFavicon(newIsDark);
+
+      return newIsDark;
+    });
   };
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "light") {
-      document.body.classList.remove("dark");
-      setIsDark(false);
-    } else {
-      document.body.classList.add("dark");
-      setIsDark(true);
-    }
+    const savedTheme = localStorage.getItem(localStorageKey);
+    const isSavedDark = savedTheme !== "light";
+    document.body.classList.toggle("dark", isSavedDark);
+    setIsDark(isSavedDark);
+
+    // Set favicon based on saved theme preference
+    updateFavicon(isSavedDark);
   }, []);
 
   return (
@@ -33,7 +47,6 @@ function ThemeSwitcher() {
       onClick={changeTheme}
       className="text-2xl text-medium-gray dark:text-white-85 hover:text-accent duration-short cursor-pointer"
     >
-      {/* {isDark ? <IoSunnyOutline /> : <IoMoonOutline />} */}
       {isDark ? sunIcon : moonIcon}
     </div>
   );
