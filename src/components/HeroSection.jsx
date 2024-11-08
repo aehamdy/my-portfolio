e134f9f56cf9f16cfdbdcd97b443b7b09adc9be7
aehamdy <ae.hamdy91@gmail.com>
2024-11-08 19:11:16 +0200

    feat: Add animation effect to the HeroSecion's image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,13 @@
-import myImage from "../assets/my-image.jpg";
+// import myImage from "../assets/my-image.jpg";
 import ProfileInfoSection from "./ProfileInfoSection";
 
 function HeroSection() {
   return (
     <section
-      style={{ backgroundImage: `url(${myImage})` }}
-      className={`hero-section relative flex justify-center w-full h-[calc(100dvh-109px)] md:h-[100dvh] lg:h-[90dvh] bg-cover bg-no-repeat bg-center md:bg-top rounded-b-main-section md:rounded-main-section overflow-hidden`}
+      // style={{ backgroundImage: `url(${myImage})` }}
+      className={`hero-section glitch-wrap relative flex justify-center w-full h-[calc(100dvh-109px)] md:h-[100dvh] lg:h-[90dvh] bg-cover bg-no-repeat bg-center md:bg-top rounded-b-main-section md:rounded-main-section overflow-hidden`}
     >
+      <div className="glitch"></div>
       <ProfileInfoSection />
     </section>
   );
