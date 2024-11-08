661d54525dc2cbadcd2cec848a32da9cc7ba839b
aehamdy <ae.hamdy91@gmail.com>
2024-11-08 23:35:35 +0200

    Feat: Remove glitch animation effect

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,10 @@
-// import myImage from "../assets/my-image.jpg";
+import myImage from "../assets/my-image.jpg";
 import ProfileInfoSection from "./ProfileInfoSection";
 
 function HeroSection() {
   return (
     <section
-      // style={{ backgroundImage: `url(${myImage})` }}
+      style={{ backgroundImage: `url(${myImage})` }}
       className={`hero-section glitch-wrap relative flex justify-center w-full h-[calc(100dvh-109px)] md:h-[100dvh] lg:h-[90dvh] bg-cover bg-no-repeat bg-center md:bg-top rounded-b-main-section md:rounded-main-section overflow-hidden`}
     >
       <div className="glitch"></div>
