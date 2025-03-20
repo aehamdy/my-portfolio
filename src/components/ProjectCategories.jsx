4c49a063b02cdfa7685e7d86bc3d98313a99da1a
aehamdy <ae.hamdy91@gmail.com>
2025-03-20 17:19:35 +0200

    Refactor: Map over categories object to list all categories

--- src/components/ProjectCategories.jsx
+++ src/components/ProjectCategories.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
-import { projectCategories } from "../data/projects";
+import { categories } from "../data/categories";
 import ProjectCategoriesButton from "./ProjectCategoriesButton";
 
 function ProjectCategories({ setSelectedCategory, selectedCategory }) {
+  const categoriesArray = Object.values(categories);
   return (
     <div className="flex items-center gap-4 ms-2 px-3 md:px-section-padding">
-      {projectCategories.map((item, i) => (
+      {categoriesArray.map((item, i) => (
         <ProjectCategoriesButton
           key={i}
           item={item}
