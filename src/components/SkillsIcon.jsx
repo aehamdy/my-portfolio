f619127805af5ef1eca8d4b67234ded4bd15ba87
aehamdy <ae.hamdy91@gmail.com>
2025-03-20 17:23:04 +0200

    Style: Change icon value cursor to default cursor

--- src/components/SkillsIcon.jsx
+++ src/components/SkillsIcon.jsx
@@ -6,7 +6,7 @@ function SkillsIcon({ skill }) {
       <span className="p-1 text-3xl text-accent bg-transparent-bg rounded-lg">
         {skill.icon}
       </span>
-      <span className="absolute z-10 mt-1 py-[2px] px-[4px] text-sm text-white-85 bg-body-dark rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible group-hover:translate-y-9 transition-all duration-short ease-in-out -translate-y-2">
+      <span className="absolute z-10 mt-1 py-[2px] px-[4px] text-sm text-white-85 bg-body-dark rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible group-hover:translate-y-9 transition-all duration-short ease-in-out -translate-y-2 cursor-default">
         {skill.value}
       </span>
     </li>
