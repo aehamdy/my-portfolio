a301104a0970d5d794b5e4d6f98cd7bba3d83767
AhmedEssam <ae.devverse@gmail.com>
2025-05-10 02:25:55 +0300

    Chore: Replace word file name's 'cv' with 'resume'

--- src/data/downloadWordFile.js
+++ src/data/downloadWordFile.js
@@ -3,11 +3,13 @@ import { saveAs } from "file-saver";
 import personalInfo from "./personalInfo";
 
 const downloadWordFile = async (doc) => {
+  // Save the document as a .docx file
+  const blob = await Packer.toBlob(doc); // Changed toBlob for browser environment
 
-    // Save the document as a .docx file
-    const blob = await Packer.toBlob(doc); // Changed toBlob for browser environment
-
-    saveAs(blob, `${personalInfo.firstName}${personalInfo.middleName}'s_CV.docx`); // Use saveAs to download the blob as a file
-}
+  saveAs(
+    blob,
+    `${personalInfo.firstName}${personalInfo.middleName}'s_Resume.docx`
+  ); // Use saveAs to download the blob as a file
+};
 
 export default downloadWordFile;
