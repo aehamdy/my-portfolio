a00ff54581d7cc9b016b7db393ec30214de7961b
AhmedEssam <ae.devverse@gmail.com>
2025-05-09 13:00:59 +0300

    Chore: Adjust languages to be rendered correctly after edits in personalInfo.js

--- src/data/aboutMeList.js
+++ src/data/aboutMeList.js
@@ -1,9 +1,9 @@
 import personalInfo from "./personalInfo";
 
 const aboutMeList = [
-    {residence: personalInfo.residence},
-    {location: personalInfo.location},
-    {languages: personalInfo.languages},
-]
+  { residence: personalInfo.residence },
+  { location: personalInfo.location },
+  { languages: personalInfo.languages.map((language) => language.name) },
+];
 
 export default aboutMeList;
