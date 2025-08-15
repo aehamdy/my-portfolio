56bb70806929469c6ad6fa34f3c94664b215e0a6
AhmedEssam <ae.devverse@gmail.com>
2025-08-15 18:41:44 +0300

    Chore: Update experience list by adding Tech-Cell's internship

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,25 +1,36 @@
 const experiences = [
-    {
-        heading: 'Freelance Frontend Developer',
-        dateFrom: '2024',
-        dateTo: 'present',
-        content: [
-            'Built websites and SPAs with HTML, CSS, JS, and React.',
-            'Created responsive, optimized web pages with cross-browser support.',
-            'Worked with clients to gather requirements and deliver solutions.',
-            'Used SASS for better styling and reusable code.'
-        ]
-    },
-    {
-        heading: 'Personal Projects',
-        dateFrom: '2024',
-        dateTo: 'present',
-        content: [
-            'Built a Todo App with filters and categories using React and localStorage.',
-            'Created a Pomodoro Timer with customizable cycles for productivity.',
-            'Developed a Weather App showing real-time data via API.'
-        ]
-    },
+  {
+    heading: "Freelance Frontend Developer",
+    dateFrom: "2024",
+    dateTo: "present",
+    content: [
+      "Built websites and SPAs with HTML, CSS, JS, and React.",
+      "Created responsive, optimized web pages with cross-browser support.",
+      "Worked with clients to gather requirements and deliver solutions.",
+      "Used SASS for better styling and reusable code.",
+    ],
+  },
+  {
+    heading: "Internship @ Tech-Cell",
+    dateFrom: "Apr 2025",
+    dateTo: "May 2025",
+    content: [
+      "Delivered fully functional, responsive UIs for Car Rental, E-Commerce, and E-Learning platforms using React, TypeScript, Tailwind CSS, Redux Toolkit, and Shadcn UI.",
+      "Built reusable, modular components and implemented efficient state management, reducing development time across projects.",
+      "Collaborated closely with cross-functional teams, ensuring smooth integration between frontend, backend, and QA processes.",
+      "Completed all frontend deliverables ahead of deadlines, maintaining high code quality despite incomplete backend implementations.",
+    ],
+  },
+  {
+    heading: "Personal Projects",
+    dateFrom: "2024",
+    dateTo: "present",
+    content: [
+      "Built a Todo App with filters and categories using React and localStorage.",
+      "Created a Pomodoro Timer with customizable cycles for productivity.",
+      "Developed a Weather App showing real-time data via API.",
+    ],
+  },
 ];
 
 export default experiences;
