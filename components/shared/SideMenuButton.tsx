eeaf18d31d9901beb6e2ddaf3303e464ca737b2d
AhmedEssam <ae.devverse@gmail.com>
2026-04-03 00:57:56 +0200

    Feat: Create Logo, Header, Footer, SideMenuButton and Icon components

---
+++ components/shared/SideMenuButton.tsx
@@ -0,0 +1,11 @@
+import Icon from "./Icon";
+
+function SideMenuButton() {
+  return (
+    <button className="">
+      <Icon name="Menu" size={28} className="text-black" />
+    </button>
+  );
+}
+
+export default SideMenuButton;
